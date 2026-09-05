952eb082900f3c5fdc88efb560be4b6919567ac1
cloudwolf <206946888+pocketradio@users.noreply.github.com>
2026-09-05 19:39:55 +0530

    centralize bittorrent message framing and request encoding

--- internal/peers/send.go
+++ internal/peers/send.go
@@ -19,19 +19,36 @@ func CheckInterest(conn net.Conn, pState *PeerState, clientMgr *piece.ClientMana
 }
 
 func SendInterested(conn net.Conn) error {
-	message := []byte{0, 0, 0, 1, 2} // 4 byte length, 1 byte ID ( = 2), no payload
-	_, err := conn.Write(message)
-	return err
+	return sendMessage(conn, 2, nil)
 }
 
 func SendRequest(conn net.Conn, index uint32, begin uint32, length uint32) error {
-	message := make([]byte, 17)
-	binary.BigEndian.PutUint32(message[0:4], 13)
-	message[4] = 6
-	binary.BigEndian.PutUint32(message[5:9], index)
-	binary.BigEndian.PutUint32(message[9:13], begin)
-	binary.BigEndian.PutUint32(message[13:17], length)
+	payload := make([]byte, 12)
+	binary.BigEndian.PutUint32(payload[0:4], index)
+	binary.BigEndian.PutUint32(payload[4:8], begin)
+	binary.BigEndian.PutUint32(payload[8:12], length)
 
-	_, err := conn.Write(message)
-	return err
+	return sendMessage(conn, 6, payload)
+}
+
+// (generic helper fn)
+
+func sendMessage(conn net.Conn, id byte, payload []byte) error {
+
+	message := make([]byte, 5+len(payload)) // length = 4 bytes, ID = 1 byte.
+	binary.BigEndian.PutUint32(message[0:4], uint32(1+len(payload)))
+	message[4] = id // first 4 bytes for length
+
+	copy(message[5:], payload)
+
+	// in case of req
+
+	for len(message) > 0 {
+		n, err := conn.Write(message)
+		if err != nil {
+			return err
+		}
+		message = message[n:]
+	}
+	return nil
 }
